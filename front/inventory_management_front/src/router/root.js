e82e12ab6ea9710053da3b5386fa2d160edb7713
kang-dayeon <dksldiss89@gmail.com>
2024-06-11 18:09:00 +0900

    Modify router

--- front/inventory_management_front/src/router/root.js
+++ front/inventory_management_front/src/router/root.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import DashboardPage from '../pages/DashboardPage';
+import Product from '../pages/Product';
 
 const root = createBrowserRouter ([
   {
     path: "",
     element: <DashboardPage />
+  },
+  {
+    path: "/product",
+    element: <Product />
   }
 ]);
 
