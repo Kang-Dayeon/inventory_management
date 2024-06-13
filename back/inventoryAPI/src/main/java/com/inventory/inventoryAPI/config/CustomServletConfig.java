5a37231c7ce0108c491ad05b7eb2687aecd9e3e2
kang-dayeon <dksldiss89@gmail.com>
2024-06-13 17:09:03 +0900

    Create core setting

---
+++ back/inventoryAPI/src/main/java/com/inventory/inventoryAPI/config/CustomServletConfig.java
@@ -0,0 +1,23 @@
+package com.inventory.inventoryAPI.config;
+
+import com.inventory.inventoryAPI.controller.formatter.LocalDateFormetter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CustomServletConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new LocalDateFormetter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .maxAge(500)
+                .allowedMethods("GET","POST","PUT","DELETE","HEAD","OPTIONS")
+                .allowedOrigins("*");
+    }
+}
