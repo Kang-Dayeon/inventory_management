2012cc7e4fa6735e523a3504d80c777cab19a7b8
kang-dayeon <dksldiss89@gmail.com>
2024-06-20 18:24:10 +0900

    Modify modify page router 追加

--- front/inventory_management_front/src/router/supplierRouter.js
+++ front/inventory_management_front/src/router/supplierRouter.js
@@ -5,6 +5,7 @@ import { Navigate } from 'react-router-dom';
 const Loading = <div className='container-fluid d-flex justify-content-center'><Spinner>Loading...</Spinner></div>
 const SupplierList = lazy(() => import("../pages/supplier/ListPage"))
 const SupplierAdd = lazy(() => import("../pages/supplier/AddPage"))
+const SupplierModify = lazy(() => import("../pages/supplier/ModifyPage"))
 
 const supplierRouter = () => {
   return [
@@ -19,6 +20,10 @@ const supplierRouter = () => {
     {
       path: "add",
       element: <Suspense fallback={Loading}><SupplierAdd /></Suspense>
+    },
+    {
+      path: ":supplierId",
+      element: <Suspense fallback={Loading}><SupplierModify /></Suspense>
     },
   ];
 };
