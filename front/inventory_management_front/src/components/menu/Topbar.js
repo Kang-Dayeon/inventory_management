b9c84658df341396edcf77dba4e7e3e334085cbc
kang-dayeon <dksldiss89@gmail.com>
2024-06-28 13:55:55 +0900

    Modify header

--- front/inventory_management_front/src/components/menu/Topbar.js
+++ front/inventory_management_front/src/components/menu/Topbar.js
@@ -1,32 +1,54 @@
 import React, { useState } from "react";
 import {
   Navbar,
-  NavbarToggler,
+  Tooltip,
+  NavbarBrand,
 } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faRightFromBracket,
+  faCartFlatbed
 } from "@fortawesome/free-solid-svg-icons";
 import useCustomLogin from "../../hooks/useCustomLogin";
 
 const Topbar = () => {
   const {doLogout, moveToLogin} = useCustomLogin()
+  const {loginState} = useCustomLogin()
+
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+  const toggle = () => setTooltipOpen(!tooltipOpen);
 
   const handleClickLogout = () => {
     doLogout()
     moveToLogin()
   }
 
+
   return (
     <Navbar
       color="light"
       light
-      className="navbar shadow-sm p-3 mb-5 bg-white rounded d-flex justify-content-end"
+      className="navbar shadow-sm p-3 mb-5 bg-white rounded d-flex justify-content-end align-items-center"
       expand="md"
     >
-      <button className="icon-btn" onClick={handleClickLogout}>
-       <FontAwesomeIcon icon={faRightFromBracket} />
-      </button>
+      <NavbarBrand href="/" className="header-icon">
+        <FontAwesomeIcon icon={faCartFlatbed} />
+      </NavbarBrand>
+      <div className="d-flex justify-content-center align-items-center header-right">
+        <p class="mb-0 font-weight-bold">{loginState.username} 様</p>
+        <button className="icon-btn logout ml-2" onClick={handleClickLogout} id="logout">
+          <FontAwesomeIcon icon={faRightFromBracket} />
+        </button>
+        <Tooltip
+          placement="bottom"
+          isOpen={tooltipOpen}
+          target="logout"
+          toggle={toggle}
+          autohide={false}
+        >
+          LOGOUT
+        </Tooltip>
+      </div>
       
     </Navbar>
   );
