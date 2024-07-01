2f9acf0cc396c1f12599fb3a2a5a96cb1a25b5c5
kang-dayeon <dksldiss89@gmail.com>
2024-07-01 22:48:23 +0900

    modify layout

--- front/inventory_management_front/src/layouts/MainLayout.js
+++ front/inventory_management_front/src/layouts/MainLayout.js
@@ -3,13 +3,10 @@ import SideBar from '../components/menu/Sidebar';
 import Topbar from '../components/menu/Topbar';
 import { Container } from "reactstrap";
 import classNames from "classnames";
-import LoadingComponent from '../components/common/LoadingComponent';
-import useCustomLoading from '../hooks/useCustomLoading';
 
 const MainLayout = ({children}) => {
   const [sidebarIsOpen, setSidebarOpen] = useState(true);
   const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
-  const {isLoading}= useCustomLoading()
   
   return (
     <div className='wrapper'>
@@ -19,11 +16,7 @@ const MainLayout = ({children}) => {
         className={classNames("content", { "is-open": sidebarIsOpen })}
       >
         <Topbar/>
-        {isLoading ? (
-          <LoadingComponent />
-        ): (
-          children
-        )}
+        {children}
       </Container>
     </div>
   );
